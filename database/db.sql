842f9ebacf3d016b3e7d88bd1ebf27a80b088aed
ivansmb11 <ismb9@hotmail.com>
2022-03-05 18:36:55 -0600

    ubuntu

--- database/db.sql
+++ database/db.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE 
+CREATE DATABASE application;
+
+USE application;
+
+CREATE TABLE users(
+    id INTEGER(11) PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    full_name VARCHAR(100) NOT NULL
+)
+
+DESCRIBE users;
